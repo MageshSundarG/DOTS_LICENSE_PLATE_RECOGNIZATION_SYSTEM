874cebd5a54564bb865d415bc67969bd0c0c5390
MAGESH SUNDAR G <60824988+MageshSundarG@users.noreply.github.com>
2020-11-01 01:05:40 +0530

    Update default.py

--- Flask_Web_App/config/default.py
+++ Flask_Web_App/config/default.py
@@ -1,3 +1,3 @@
-MONGO_CLIENT="mongodb+srv://admin:admin@cluster.7ebff.mongodb.net/flaskapp?retryWrites=true&w=majority"
-MAIL_USERNAME_CONFIG = 'goutham.bake@gmail.com'
-MAIL_PASSWORD_CONFIG = 'itsgoutham12@'
+MONGO_CLIENT=""
+MAIL_USERNAME_CONFIG = ''
+MAIL_PASSWORD_CONFIG = ''
